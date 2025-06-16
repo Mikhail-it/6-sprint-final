4e9793a60337992f13c7cef27db37dcaa1d2fde4
Михаил <misha.lozhkin.95@gmail.com>
2025-06-16 17:17:42 +0400

    uno commit

--- internal/server/server.go
+++ internal/server/server.go
@@ -1,1 +1,31 @@
 package server
+
+import (
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/Yandex-Practicum/go1fl-sprint6-final/internal/handlers"
+	"github.com/go-chi/chi"
+)
+
+type Server struct {
+	Log        *log.Logger
+	HttpServer *http.Server
+}
+
+func NewServer(log *log.Logger) *Server {
+	r := chi.NewRouter()
+	r.Get("/", handlers.IndexHandler)
+	r.Post("/upload", handlers.UploadHandler)
+
+	httpServer := &http.Server{
+		Addr:         ":8800",
+		Handler:      r,
+		ErrorLog:     log,
+		ReadTimeout:  10 * time.Second,
+		WriteTimeout: 20 * time.Second,
+		IdleTimeout:  30 * time.Second,
+	}
+	return &Server{Log: log, HttpServer: httpServer}
+}
