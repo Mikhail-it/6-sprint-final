cac27ba45cb79b9bb3a8a4bdcafcccf2a10e2e14
Михаил <misha.lozhkin.95@gmail.com>
2025-06-17 20:01:45 +0400

    10

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -54,4 +54,6 @@ func UploadHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
 	w.WriteHeader(http.StatusOK)
 	w.Write([]byte(converted))
+	http.HandleFunc("/", IndexHandler)
+	http.HandleFunc("/upload", UploadHandler)
 }
